dca26f089ef408831ae4e1bcfd07c914736debfe
huangtianyang <403622098@qq.com>
2019-10-19 23:21:34 +0800

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Map } from 'react-amap';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ width: '100%', height: 500 }}>
+        <CssBaseline />
+        <Map amapkey="35412c4cea7061933cad3b26eef171db" />
+      </div>
+    </>
   );
 }
 
